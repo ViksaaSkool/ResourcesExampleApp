3a05519d665d1469cd2291bcf80ea7f3ab8a49d9
ViksaaSkool <victor.arsovski@gmail.com>
2020-11-13 02:59:46 +0100

    done

--- app/src/main/java/com/viksaa/resources/app/ui/other/OtherFragment.kt
+++ app/src/main/java/com/viksaa/resources/app/ui/other/OtherFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatToggleButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.viksaa.resources.app.R
+import java.util.*
 
 class OtherFragment : Fragment() {
 
@@ -22,22 +26,30 @@ class OtherFragment : Fragment() {
         otherViewModel =
             ViewModelProvider(this).get(OtherViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_other, container, false)
-
+        setButton(root)
+        setThemeChanger(root)
         return root
     }
 
 
-    /*fun setAppearanceTheme(theme: Int) {
-        when (theme) {
-            AppAppearance.LIGHT -> {
+    private fun setButton(view: View){
+        val button = view.findViewById<AppCompatButton>(R.id.example_button)
+        val messages = requireContext().resources.getStringArray(R.array.button_messages_text)
+        button.setOnClickListener {
+            val number = Random().nextInt(messages.size-1)
+            val message = String.format(requireContext().getString(R.string.toast_text), messages[number], number)
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setThemeChanger(view: View){
+        val themeSwitch = view.findViewById<AppCompatToggleButton>(R.id.theme_switch)
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
-            AppAppearance.DARK -> {
+            } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             }
-            AppAppearance.FOLLOW_SYSTEM -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-            }
         }
-    }*/
+    }
 }
